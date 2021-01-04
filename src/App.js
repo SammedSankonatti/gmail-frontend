bd674b96dfb53a7286b5bd66cd14e10747fa6d1d
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-04 09:11:06 +0530

    The react router dom is now added to project

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './App.css';
 
 import Header from './Header/Header'
-
 import Sidebar from './Sidebar';
+
+import {BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import Mail from './Mail';
+import EmailList from './EmailList';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Header />
-      <Sidebar />
+      <div className="app__body">
+        <Sidebar />
+        <Switch>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/EmailList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EmailList.css';
+
+function EmailList() {
+    return (
+        <div className="emailList">
+            
+        </div>
+    )
+}
+
+export default EmailList
