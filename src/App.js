048e63d10b98700126f1fe4ac9c3a5a2bb10d511
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-04 06:34:17 +0530

    the part of the header is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header'
+
 function App() {
   return (
     <div className="App">
-      <h3>Gmail</h3>
+      <Header />
     </div>
   );
 }
