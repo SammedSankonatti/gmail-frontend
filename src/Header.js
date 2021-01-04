0c5479099ac7a5e99e00ecd160a9eb621dab8469
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-04 06:43:41 +0530

    Pretty much done with header component

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,15 @@ import SearchIcon from '@material-ui/icons/Search';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
-import {Avatar} from '@material-ui/core';
+import {Avatar , IconButton} from '@material-ui/core';
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <MenuIcon />
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
                 <img src="https://www.logo.wine/a/logo/Gmail/Gmail-Logo.wine.svg" alt="gmail-logo" />
             </div>
             <div className="header__middle">
@@ -21,8 +23,12 @@ function Header() {
                 <ArrowDropDownIcon  className="header__inputCaret" />
             </div>
             <div className="header__right">
-                <AppsIcon />
-                <NotificationsActiveIcon />
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
                 <Avatar />
             </div>
         </div>
