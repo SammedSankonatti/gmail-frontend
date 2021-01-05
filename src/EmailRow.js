79c9f5dcc5654baccd55a73c2f353f41fc312780
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-05 06:38:12 +0530

    Time for jogging

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -5,10 +5,14 @@ import { Checkbox } from '@material-ui/core';
 import {IconButton} from '@material-ui/core';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import LabelImportantOutlinedIcon from '@material-ui/icons/LabelImportantOutlined';
+import { useHistory } from 'react-router-dom';
 
 function EmailRow( { id, subject,title,description,time } ) {
+
+    const history= useHistory();
+
     return (
-        <div className="emailRow">
+        <div onClick={()=> history.push("/mail") } className="emailRow">
             <div className="emailRow__options">
                 <Checkbox />
                 <IconButton>
