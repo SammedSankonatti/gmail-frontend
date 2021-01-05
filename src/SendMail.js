debc755d55c15e491be1509d49b9768604c4fa07
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-05 10:39:14 +0530

    Final part is left now i,e firebase integration and etc

--- src/SendMail.js
+++ src/SendMail.js
@@ -23,7 +23,7 @@ function SendMail() {
                 <CloseIcon className="sendMail__close" onClick={()=> dispatch(closeSendMessage()) } />
             </div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input  name="to" placeholder="To:" type="text" ref={register({required:true})} />
+                <input  name="to" placeholder="To:" type="email" ref={register({required:true})} />
                 {errors.to&& <p> To is required </p> }
                  <input name="subject"  placeholder="Subject" type="text"  ref={register({required:true})} />
                   <input  name="message" className="sendMail__message" placeholder="Message ..." type="text"  ref={register({required:true})} />
