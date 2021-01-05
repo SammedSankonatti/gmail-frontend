9152906177e7a1b27c3fb3949f12002f23c7224e
sammed-sankonatti <myofficialtrack@gmail.com>
2021-01-05 18:09:09 +0530

    Successfully sent the first message on firebase database

--- src/SendMail.js
+++ src/SendMail.js
@@ -7,13 +7,23 @@ import { Button } from '@material-ui/core';
 import {useForm}from "react-hook-form";
 import { useDispatch } from 'react-redux';
 import { closeSendMessage } from './features/mailSlice';
+import {db} from './firebase';
+import firebase from 'firebase';
 
 function SendMail() {
 
     const {register, handleSubmit, watch, errors}  = useForm();
     const dispatch= useDispatch();
-    const onSubmit=(data)=>{
-        console.log(data);
+
+    const onSubmit=(formData)=>{
+        console.log(formData);
+        db.collection('emails').add({
+            to: formData.to,
+            subject:formData.subject,
+            message: formData.message,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        dispatch(closeSendMessage());
     }
 
     return (
@@ -26,8 +36,9 @@ function SendMail() {
                 <input  name="to" placeholder="To:" type="email" ref={register({required:true})} />
                 {errors.to&& <p> To is required </p> }
                  <input name="subject"  placeholder="Subject" type="text"  ref={register({required:true})} />
+                 {errors.subject&& <p> subject is required </p> }
                   <input  name="message" className="sendMail__message" placeholder="Message ..." type="text"  ref={register({required:true})} />
-
+                    {errors.message&& <p> message is required </p> }
                   <div className="sendMail__options">
                       <button className="sendMail__send" >Send</button>
                   </div>
